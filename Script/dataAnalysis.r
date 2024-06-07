75d323431fc4b7dc93d789934d09bd2162419811
Laif Dzung <97878200+DzungLaif@users.noreply.github.com>
2024-06-08 00:43:41 +0300

    Update dataAnalysis.r

--- Script/dataAnalysis.r
+++ Script/dataAnalysis.r
@@ -35,7 +35,7 @@ pDat <- pDat %>%mutate(Age_bracket = case_when(
                        between(Age, 41, 60) ~ "41-60",
                        Age > 60 ~ "above_60"))
 pDat$Age_bracket <- as.factor(pDat$Age_bracket)
-pDat %>%count(COVID, Age_bracket)
+pDat %>% count(COVID, Age_bracket)
 
 # Plot graphs to visualize these separation
 pDat %>% 
@@ -79,7 +79,48 @@ e_fil <- eDat %>%
 # Normalize data by relative-Log Expression method
 genes <- as.data.frame(row.names(e_fil))
 norm <- DGEList(counts = e_fil2, samples = pDat, genes = genes, group = pDat$COVID)
-eNorm <- calcNormFactors(norm, method = "RLE") 
-eNorm <- cpm(eNorm)
-eNorm <- as.data.frame(eNorm)
+eNormz <- calcNormFactors(norm, method = "RLE") 
+eNormz <- cpm(eNorm)
+eNormz <- as.data.frame(eNorm)
 
+# Loading eNorm
+eNorm <- read.delim(here::here("data", "eNorm.txt"), sep = "\t")
+eNorm <- eNorm %>% column_to_rownames(var = "gene")
+pDat <- read.delim(here::here("data", "GSE157103_formatted_pDat.txt"), sep = "\t")
+pDat <- pDat %>% column_to_rownames(var = "ID")
+
+# Transforming eNorm values to log2(x)+1 and transposing the result, then caculate PCA.
+e_log2 <- log2(eNorm + 1)
+t_log2 <- as.data.frame(t(e_log2))
+pca <- prcomp(t_log2, scale = FALSE, center = TRUE)
+summary <- data.frame(PC = 1:126, var_explained = (pca$sdev)^2 / sum((pca$sdev)^2), 
+                      cumulative = cumsum(pca$sdev^2 / sum(pca$sdev^2)))
+summary <- summary %>% mutate(cumulative_perc = cumulative*100)
+summary <- summary[1:30,]
+scores <- as.data.frame(pca$x)
+scores <- scores[c(1:30)]
+
+# Adding PCA scores into pDat dataFrames.
+mDat <- cbind(pDat, scores)
+
+# variation analysis 
+variable_variance <- lmmatrix(dep = scores, ind = pDat[c(2:15)], metric = "Pvalue")
+vv_plot <- variable_variance %>% as.data.frame() 
+vv_plot <- as.data.frame(t(vv_plot))
+vv_plot <- vv_plot %>% 
+  mutate(Principle_Component = 1:30) %>% 
+  dplyr::select(Principle_Component, everything())
+vv_plot <- vv_plot %>% 
+  pivot_longer(cols = -c(Principle_Component), names_to = "variables", values_to = "pval") 
+vv_plot <- vv_plot %>% 
+  mutate(pval_cat = case_when(
+    pval > 0.05  ~ "> 0.05",
+    pval < 0.05 & pval > 0.01 ~ "< 0.05",
+    pval < 0.01 & pval > 0.001 ~ "< 0.01",
+    pval < 0.001 ~ "< 0.001"
+  ))
+vv_plot %>% 
+  ggplot(aes(x = Principle_Component, y = variables, fill = pval_cat)) +
+  geom_tile() + 
+  theme_bw() +
+  labs(x = "PC", y = "Variables" , fill = "P value")
